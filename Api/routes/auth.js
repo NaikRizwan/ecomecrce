7c9bef9a378bc1e4175b20d9259ce3cc5f11bc35
dayan52 <110128560+NaikRizwan@users.noreply.github.com>
2023-11-09 23:25:59 +0530

    secure

--- Api/routes/auth.js
+++ Api/routes/auth.js
@@ -95,6 +95,7 @@ router.get("/about", authh, (req, res) => {
 router.get("/logout", authh, (req, res) => {
   res.clearCookie("jwtoken", {
     path: "/",
+    secure: true,
     sameSite: "none",
   });
   res.status(200).send("user logout");
