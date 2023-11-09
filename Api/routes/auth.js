560e342ded27fa79b658deccd6fe091d52291901
dayan52 <110128560+NaikRizwan@users.noreply.github.com>
2023-11-09 16:15:23 +0530

    auth.js

--- Api/routes/auth.js
+++ Api/routes/auth.js
@@ -1,22 +1,19 @@
 // routes/auth.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User'); // Import the User model
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const authh =require("../middleware/authh");
-const Orrder = require('../models/Orrder');
-router.post('/reg', async (req, res) => {
-
-
+const User = require("../models/User"); // Import the User model
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const authh = require("../middleware/authh");
+const Orrder = require("../models/Orrder");
+router.post("/reg", async (req, res) => {
   try {
-
     const { full_name, email, password } = req.body;
 
     if (!full_name || !email || !password) {
       return res.status(422).json({ error: "plz filled all field" });
     }
-    const Userexit = await User.findOne({ email: email })
+    const Userexit = await User.findOne({ email: email });
     if (Userexit) {
       return res.status(422).json({ error: "email already exist" });
     }
@@ -24,15 +21,14 @@ router.post('/reg', async (req, res) => {
     const user = new User({ full_name, email, password });
     await user.save();
 
-
-    res.status(201).json({ message: 'Registration successful' });
+    res.status(201).json({ message: "Registration successful" });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Registration failed' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Registration failed" });
   }
 });
-router.post('/log', async (req, res) => {
-console.log("log is calling");
+router.post("/log", async (req, res) => {
+  console.log("log is calling");
   try {
     let token;
     const { email, password } = req.body;
@@ -41,94 +37,74 @@ console.log("log is calling");
     }
     const userlogin = await User.findOne({ email: email });
     if (userlogin) {
-    const ismatch = await bcrypt.compare(password, userlogin.password);
-    token = await userlogin.generateAuthToken();
-    
+      const ismatch = await bcrypt.compare(password, userlogin.password);
+      token = await userlogin.generateAuthToken();
 
-    
- 
-res.cookie("jwtoken", token,{
-  expires:new Date(Date.now() + 25892000000),
-  httpOnly:true
-})
+      res.cookie("jwtoken", token, {
+        expires: new Date(Date.now() + 25892000000),
+        httpOnly: true,
+      });
 
-   
       if (!ismatch) {
-
-
         res.status(400).json({ message: "invalid carendatinals " });
-      }
-      else {
-        res.status(200).json({ 
+      } else {
+        res.status(200).json({
           message: "user login succesfully",
-          userlogin:userlogin
-         });
+          userlogin: userlogin,
+        });
+        res.header("Access-Control-Allow-Origin", "*");
       }
     } else {
       res.status(400).json({ message: "invalid carendatinals " });
     }
+  } catch (error) {
+    console.log("Error:", error);
+    res.status(500).json({ error: "login failed" });
   }
-  catch (error) {
-    console.log('Error:', error);
-    res.status(500).json({ error: 'login failed' });
-  }
-})
-router.get('/riz',authh ,(req ,res) => {
- 
-    res.send(req.rootUser);
-  })
-  // router.get('/rifat',authh ,(req ,res) => {
-  //   console.log("nav calling");
-     
-  //   })
-  router.get('/rifat', authh, (req, res) => {
-    
-    res.json({ userDataa: true });
-  });
-router.get('/about',authh ,(req ,res) => {
-console.log("about");
+});
+router.get("/riz", authh, (req, res) => {
   res.send(req.rootUser);
-})
+});
+// router.get('/rifat',authh ,(req ,res) => {
+//   console.log("nav calling");
 
-router.get('/logout', authh,(req,res)=>{
+//   })
+router.get("/rifat", authh, (req, res) => {
+  res.json({ userDataa: true });
+});
+router.get("/about", authh, (req, res) => {
+  console.log("about");
+  res.send(req.rootUser);
+});
 
-res.clearCookie("jwtoken", {path : '/'});
-res.status(200).send('user logout');
+router.get("/logout", authh, (req, res) => {
+  res.clearCookie("jwtoken", { path: "/" });
+  res.status(200).send("user logout");
+});
 
-})
-
-router.post('/myorder',async (req,res) =>{
-    try{
-      console.log("myorser is calling");
-    const {email ,full_name} =req.body;
-     console.log(email,full_name);
+router.post("/myorder", async (req, res) => {
+  try {
+    console.log("myorser is calling");
+    const { email, full_name } = req.body;
+    console.log(email, full_name);
     const usermyorder = await Orrder.find({ email });
     res.status(200).json({
-     
       orders: usermyorder,
-    
     });
-    }
-    
-    catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'An error occurred' });
-    }
-    
-    
-    
-    });
-    router.post('/orders', async (req, res) => {
-        try {
-          const order = new Orrder(req.body);
-          await order.save();
-          
-          
-          res.status(201).json({ message: 'User order succefully successfully' });
-         
-        } catch (error) {
-          console.error('Error placing order:', error);
-          res.status(500).send('An error occurred. Please try again later.');
-        }
-      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+});
+router.post("/orders", async (req, res) => {
+  try {
+    const order = new Orrder(req.body);
+    await order.save();
+
+    res.status(201).json({ message: "User order succefully successfully" });
+  } catch (error) {
+    console.error("Error placing order:", error);
+    res.status(500).send("An error occurred. Please try again later.");
+  }
+});
 module.exports = router;
